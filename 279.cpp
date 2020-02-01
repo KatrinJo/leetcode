5c327e9b0124ab4f66cb28979bb8d39ee198942b
KatrinJo <327548244@qq.com>
2020-02-01 10:53:26 +0800

    Create 279.cpp

---
+++ 279.cpp
@@ -0,0 +1,28 @@
+// 打表+合理剪枝
+
+class Solution {
+public:
+    int numSquares(int n) {
+        if (n < 4)
+            return n;
+        vector<int> myvec(n + 1,0);
+        myvec[1] = 1;
+        myvec[2] = 2;
+        myvec[3] = 3;
+        int sqrtNum;
+        int j = 2;
+        for (int i = 4; i <= n; ++i) {
+            if (j * j == i) {
+                myvec[i] = 1;
+                ++j;
+            }
+            else {
+                myvec[i] = i;
+                sqrtNum = sqrt(i);
+                for (int k = 2; k <= sqrtNum;++k)
+                    myvec[i] = min(myvec[i], myvec[k * k] + myvec[i - k * k]);
+            }
+        }
+        return myvec[n];
+    }
+};
